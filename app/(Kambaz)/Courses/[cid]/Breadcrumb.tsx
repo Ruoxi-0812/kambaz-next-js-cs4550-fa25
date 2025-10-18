78de813d35351859d8a57150bd8cd67fdcd4b798
Ruoxi Wang <ruoxiwang42@gmail.com>
2025-10-17 22:47:56 -0400

    a3 JavaScript

--- app/(Kambaz)/Courses/[cid]/Breadcrumb.tsx
+++ app/(Kambaz)/Courses/[cid]/Breadcrumb.tsx
@@ -5,9 +5,12 @@ import { usePathname } from "next/navigation";
 
 export default function Breadcrumb({ course }: { course: { name: string } | undefined; }) {
  const pathname = usePathname();
+ const parts = pathname.split("/").filter(Boolean);     
+  const cidPos = parts.indexOf("Courses") + 1;         
+  const section = parts[cidPos + 1] || "";   
  return (
    <span>
-     Course {course?.name} &gt; {pathname.split("/").pop()}
+     Course {course?.name} &gt; {section}
    </span>
  );
 }
