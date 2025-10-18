1bfe31b3dba39a695c66239e18591363c592b2e5
Ruoxi Wang <ruoxiwang42@gmail.com>
2025-10-17 22:32:30 -0400

    a3 JavaScript

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,23 +1,23 @@
 import type { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
-import { FaAlignJustify } from "react-icons/fa";
+import { FaAlignJustify } from "react-icons/fa6";
 import { courses } from "../../Database";
 import Breadcrumb from "./Breadcrumb";
 
-export default function CoursesLayout({
+export default async function CoursesLayout({
   children,
   params,
-}: Readonly<{ children: ReactNode; params: { cid: string } }>) {
-  const { cid } = params;
-  const course = courses.find((course) => course._id === cid);
-
+}: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
+  const { cid } = await params;                       
+  const course = courses.find((c) => c._id === cid);
 
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        <Breadcrumb course={course} />
+        {course?.name}
       </h2>
+      <Breadcrumb course={course} />
       <hr />
       <div className="d-flex">
         <div className="d-none d-md-block">
--- app/(Kambaz)/layout.tsx
+++ app/(Kambaz)/layout.tsx
@@ -2,16 +2,18 @@ import "./styles.css";
 import { ReactNode } from "react";
 import KambazNavigation from "./Navigation";
 
-export default function KambazLayout({
-  children,
-}: Readonly<{ children: ReactNode }>) {
+interface KambazLayoutProps {
+  children: ReactNode;
+}
+
+export default function KambazLayout({ children }: KambazLayoutProps) {
   return (
     <div id="wd-kambaz">
       <div className="d-flex">
         <div>
           <KambazNavigation />
         </div>
-        <div className="wd-main-content-offset p-3 flex-fill"> {children} </div>
+        <div className="wd-main-content-offset p-3 flex-fill">{children}</div>
       </div>
     </div>
   );
