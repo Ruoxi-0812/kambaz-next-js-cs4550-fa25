8000970d13d879c4509265151afd178e9b6edc96
Ruoxi Wang <ruoxiwang42@gmail.com>
2025-11-15 06:46:11 -0500

    a5

--- app/(Kambaz)/Account/Session.tsx
+++ app/(Kambaz)/Account/Session.tsx
@@ -4,18 +4,26 @@ import * as client from "./client";
 import { useEffect, useState } from "react";
 import { setCurrentUser } from "./reducer";
 import { useDispatch } from "react-redux";
-export default function Session({ children }: { children: React.ReactNode }){
+import axios from "axios";
+export default function Session({ children }: { children: React.ReactNode }) {
   const [pending, setPending] = useState(true);
   const dispatch = useDispatch();
+
   const fetchProfile = async () => {
     try {
       const currentUser = await client.profile();
       dispatch(setCurrentUser(currentUser));
-    } catch (err: unknown) {
-      console.error(err);
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response?.status === 401) {
+       
+      } else {
+        console.error(err); 
+      }
+    } finally {
+      setPending(false);
     }
-    setPending(false);
   };
+
   useEffect(() => {
     fetchProfile();
   }, []);
--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -1,38 +1,39 @@
 "use client";
 import Link from "next/link";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { setCurrentUser } from "../reducer";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+import * as db from "../../Database";
 import { FormControl, Button } from "react-bootstrap";
 import * as client from "../client";
 
-type Credentials = {
+interface User {
   username: string;
   password: string;
-};
+  [key: string]: unknown;
+}
 
 export default function Signin() {
-  const [credentials, setCredentials] = useState<Credentials>({
+  const [credentials, setCredentials] = useState<User>({
     username: "ruoxi",
     password: "123456",
   });
-
   const dispatch = useDispatch();
+  const router = useRouter();
 
-  const signin = async () => {
-    const user = await client.signin(credentials);
+const signin = async () => {
+    const user =  await client.signin(credentials);
     if (!user) return;
     dispatch(setCurrentUser(user));
-    redirect("/Dashboard");
+    router.push("/Dashboard");
   };
 
   return (
     <div id="wd-signin-screen">
       <h1>Sign in</h1>
-
       <FormControl
-        value={credentials.username}
+        defaultValue={credentials.username}
         onChange={(e) =>
           setCredentials({ ...credentials, username: e.target.value })
         }
@@ -40,9 +41,8 @@ export default function Signin() {
         placeholder="username"
         id="wd-username"
       />
-
       <FormControl
-        value={credentials.password}
+        defaultValue={credentials.password}
         onChange={(e) =>
           setCredentials({ ...credentials, password: e.target.value })
         }
@@ -51,11 +51,9 @@ export default function Signin() {
         type="password"
         id="wd-password"
       />
-
-      <Button onClick={signin} id="wd-signin-btn" className="w-100 mb-2">
+      <Button onClick={signin} id="wd-signin-btn" className="w-100">
         Sign in
       </Button>
-
       <Link id="wd-signup-link" href="/Account/Signup">
         Sign up
       </Link>
