ca3ffbeabc61f3e803a21c1d2c02e5c9f6f25189
Ruoxi Wang <ruoxiwang42@gmail.com>
2025-11-17 16:22:10 -0500

    a5

--- app/(Kambaz)/Account/Session.tsx
+++ app/(Kambaz)/Account/Session.tsx
@@ -4,29 +4,22 @@ import * as client from "./client";
 import { useEffect, useState } from "react";
 import { setCurrentUser } from "./reducer";
 import { useDispatch } from "react-redux";
-
-export default function Session({ children }: { children: React.ReactNode }) {
+export default function Session({ children }: { children: React.ReactNode }){
   const [pending, setPending] = useState(true);
   const dispatch = useDispatch();
-
   const fetchProfile = async () => {
     try {
       const currentUser = await client.profile();
       dispatch(setCurrentUser(currentUser));
-    } catch (_err) {
-      dispatch(setCurrentUser(null));
+    } catch (err: unknown) {
+      console.error(err);
     }
     setPending(false);
   };
-  
-
   useEffect(() => {
     fetchProfile();
   }, []);
-
-  if (pending) {
-    return null; // or loading spinner
+  if (!pending) {
+    return children;
   }
-
-  return children;
 }
